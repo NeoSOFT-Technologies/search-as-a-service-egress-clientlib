c37a2d014738cf92e0628cd2b7f2beef02b7a0c8
kirti.mandwade.neosoft <kirti.mandwade.neosoft@hopscotch.in>
2022-02-01 13:59:19 +0530

    added sample test case

---
+++ src/test/java/com/solr/clientwrapper/service/SearchServiceTest.java
@@ -0,0 +1,24 @@
+package com.solr.clientwrapper.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+class SearchServiceTest {
+
+	Logger logger = LoggerFactory.getLogger(SearchServiceTest.class);
+
+	
+	@Test
+	void testSolrCoreStatus() {
+		String expectedResponse = "Success";
+
+		// Get Status of Existing Core
+	
+	}
+}
